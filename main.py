1b743a180093c10554951eaaa7338f9a1d9ec161
mevitan <td.anh0812@gmail.com>
2015-07-01 01:32:51 +0700

    get movie and subtitle file name in working directory

--- main.py
+++ main.py
@@ -3,6 +3,14 @@
 
 import os, sys
 
-movie_dir = sys.argv[1] if len(sys.argv)>1 else os.getcwd()
+VIDEO_FORMAT = ('avi', 'flv', 'mkv', 'm4p', 'm4v',\
+                'mp4', 'mpeg', 'mpg', 'webm', 'wmv')
+SUBTITLE_FORMAT = ('ass', 'srt', 'ssa' ,'sub')
 
-print movie_dir
+files_in_movie_dir = os.listdir(sys.argv[1] if len(sys.argv) > 1 else os.getcwd())
+
+videos    = filter(lambda f: f.split('.')[-1] in VIDEO_FORMAT, files_in_movie_dir)
+subtitles = filter(lambda f: f.split('.')[-1] in SUBTITLE_FORMAT, files_in_movie_dir)
+
+print videos
+print subtitles
