37d95292ad8504a12cee29da8624bbd31ae7f6e0
mevitan <td.anh0812@gmail.com>
2015-07-01 21:29:16 +0700

    sort video & subtitle files, arranged corresponding to each other

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ SUBTITLE_FORMAT = ('ass', 'srt', 'ssa' ,'sub')
 
 files_in_movie_dir = os.listdir(sys.argv[1] if len(sys.argv) > 1 else os.getcwd())
 
-videos    = filter(lambda f: f.split('.')[-1] in VIDEO_FORMAT, files_in_movie_dir)
-subtitles = filter(lambda f: f.split('.')[-1] in SUBTITLE_FORMAT, files_in_movie_dir)
+videos    = sorted(filter(lambda f: f.split('.')[-1] in VIDEO_FORMAT, files_in_movie_dir))
+subtitles = sorted(filter(lambda f: f.split('.')[-1] in SUBTITLE_FORMAT, files_in_movie_dir))
 
 print videos
 print subtitles
