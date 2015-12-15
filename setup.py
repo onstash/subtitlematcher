42fd3911415a9c9b15491f2d62fea4c917cb25d6
Duc Anh Tran <td.anh0812@gmail.com>
2015-12-15 13:10:46 -0500

    add long_description

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="subtitlematcher",
     version="0.0.1",
     description="Matching videos and subtitles file names",
+    long_description=open('README.rst', 'r').read(),
     url="http://github.com/itsjef/subtitlematcher.git",
     author="Duc Anh Tran",
     author_email="td.anh0812@gmail.com",
