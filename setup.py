7722ae3f0fd1a89821c63637f02c14c88053cc22
Duc Anh Tran <td.anh0812@gmail.com>
2015-12-15 12:03:42 -0500

    minor fix

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name="subtitlematcher",
-    version="1.0",
+    version="1.0.0",
     description="Matching videos and subtitles file names",
-    url="http://github.com/itsjef/subtitlematcher",
-    author="Duc Anh",
+    url="http://github.com/itsjef/subtitlematcher.git",
+    author="Duc Anh Tran",
     author_email="td.anh0812@gmail.com",
     license='MIT',
     scripts=["src/subtitlematcher"],
